0f51a82f336767b405fe6103423547bdfb4be614
alfiyansys <alfiyan.syamsuddin@gmail.com>
2023-11-07 15:33:16 +0700

    need some test

--- clearremovedkernelmodules.sh
+++ clearremovedkernelmodules.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Get the list of installed kernel images
-installed_kernels=$(dpkg --list | grep linux-image)
+installed_kernels=$(dpkg --list | grep linux-image | grep ii)
 
 # Get the list of kernel modules
-kernel_modules=$(find /lib/modules -type d -maxdepth 1)
+kernel_modules=$(find /lib/modules -maxdepth 1 -type d -execdir basename {} \; | grep -v "modules")
 
 # Remove any kernel modules that are not in the list of installed kernel images
 for module in $kernel_modules; do
   if [[ ! $installed_kernels =~ $module ]]; then
-    echo "Removing $module"
-    #rm -rf $module
+	mod_dir="/lib/modules/$module"
+    echo "Removing $module at $mod_dir"
+    #rm -rf $mod_dir
   fi
 done
